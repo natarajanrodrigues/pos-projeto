bd350dbea213973c45c361d8173daf40977797c9
natarajanrodrigues <natarajanrodrigues@gmail.com>
2017-05-06 02:37:42 -0300

    trying deploy

--- suggestions-web/lib/run.sh
+++ suggestions-web/lib/run.sh
@@ -1,9 +1,2 @@
-./payara41/glassfish/bin/startserv
-
-./payara41/bin/asadmin create-jms-resource --restype javax.jms.ConnectionFactory jms/suggCon
-
-./payara41/bin/asadmin create-jms-resource --restype javax.jms.Queue jms/suggQueue
-
-cp target/ROOT.war ./payara/glassfish/domains/domain1/autodeploy/
-
-./payara41/bin/asadmin start-domain --verbose domain1
+create-jms-resource --restype javax.jms.ConnectionFactory jms/suggCon
+create-jms-resource --restype javax.jms.Queue jms/suggQueue
